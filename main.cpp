ef47935dcdcba98c0dc72ec4b92f12f0110b8ea9
ram-bhaskara <ramchanderbhaskara@gmail.com>
2020-07-26 16:12:13 -0500

    surface normal Image

--- main.cpp
+++ main.cpp
@@ -5,20 +5,30 @@
 
  using namespace std;
 
- bool hit_sphere(const point3& center, double radius, const ray& r) {
+ double hit_sphere(const point3& center, double radius, const ray& r) {
 	 vec3 oc = r.origin() - center;
 	 auto a = dot(r.direction(), r.direction());
 	 auto b = 2.0 * dot(oc, r.direction());
 	 auto c = dot(oc, oc) - radius * radius;
 	 auto discriminant = b * b - 4 * a * c;
-	 return (discriminant > 0);
+	 if (discriminant < 0)
+	 {
+		 return -1.0;
+	 }
+
+	 else {
+		 return (-b - sqrt(discriminant)) / (2.0 * a);
+	 }
  }
 
  color ray_color(const ray& r) {
-	 if (hit_sphere(point3(0, 0, -1), 0.5, r))
-		 return color(1, 0, 0);
+	 auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	 if (t > 0.0) {
+		 vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		 return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+	 }
 	 vec3 unit_direction = unit_vector(r.direction());
-	 auto t = 0.5 * (unit_direction.y() + 1.0);
+	 t = 0.5 * (unit_direction.y() + 1.0);
 	 return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
  }
 
@@ -47,9 +57,9 @@
 			 ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
 			 color pixel_color = ray_color(r);
 		//	 std::cout << "pixel color: " << pixel_color << "\n";
-		 write_color(std::cout, pixel_color);
+		// write_color(std::cout, pixel_color);
 
-			// std::cout << static_cast<int>(255.999 * pixel_color.x()) << ' ' << static_cast<int>(255.999 * pixel_color.y()) << ' '<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+			 std::cout << static_cast<int>(255.999 * pixel_color.x()) << ' ' << static_cast<int>(255.999 * pixel_color.y()) << ' '<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
 		 }
 	 }
 
