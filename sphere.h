69d4374f56bd5e09efb982b3e1bbbbe1eb1b7340
ram-bhaskara <ramchanderbhaskara@gmail.com>
2020-07-28 19:14:37 -0500

    main: normal and ray direction, section 6

--- sphere.h
+++ sphere.h
@@ -32,7 +32,8 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 		if (temp < t_max && temp > t_min) {
 			rec.t = temp;
 			rec.p = r.at(rec.t);
-			rec.normal = (rec.p - center) / radius;
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
 			return true;
 		}
 
@@ -40,12 +41,12 @@ bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) cons
 		if (temp < t_max && temp > t_min) {
 			rec.t = temp;
 			rec.p = r.at(rec.t);
-			rec.normal = (rec.p - center) / radius;
+			vec3 outward_normal = (rec.p - center) / radius;
+			rec.set_face_normal(r, outward_normal);
 			return true;
 		}
 	}
 
 	return false;
-}
 }
 #endif // !SPHERE_H
